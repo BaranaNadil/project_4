1849f32226b7938f15c4de209ae75efbcf80ea2c
BaranaNadil <161505829+BaranaNadil@users.noreply.github.com>
2026-01-20 18:09:03 +0530

    Main Page Design created

--- app/src/main/java/dev/barana/practical_4/activity/MainActivity.java
+++ app/src/main/java/dev/barana/practical_4/activity/MainActivity.java
@@ -7,20 +7,69 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.tabs.TabLayout;
 
 import dev.barana.practical_4.R;
+import dev.barana.practical_4.adapter.ViewPagerAdapter;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tablayout;
+
+    private ViewPager2 viewpager2;
+
+    private ViewPagerAdapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        tablayout = findViewById(R.id.tabLayout);
+        viewpager2 = findViewById(R.id.viewPager2);
+
+        tablayout.addTab(tablayout.newTab().setText("Sign Up"));
+        tablayout.addTab(tablayout.newTab().setText("Sign In"));
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+         adapter = new ViewPagerAdapter(fragmentManager, getLifecycle());
+
+         viewpager2.setAdapter(adapter);
+
+
+         tablayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+             @Override
+             public void onTabSelected(TabLayout.Tab tab) {
+                viewpager2.setCurrentItem(tab.getPosition());
+
+             }
+
+             @Override
+             public void onTabUnselected(TabLayout.Tab tab) {
+
+             }
+
+             @Override
+             public void onTabReselected(TabLayout.Tab tab) {
+
+
+             }
+         });
+
+         viewpager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+             @Override
+             public void onPageSelected(int position) {
+                 super.onPageSelected(position);
+                 tablayout.selectTab(tablayout.getTabAt(position));
+             }
+         });
+
+
     }
 }
